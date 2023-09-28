9a694527ed242fdcd7ed4e83b9aa0c0a184f043f
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-09-28 16:36:17 +0000

    Create new component ProductCard, so each card can have their own quantity state.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+const ProductCardContainer = styled.div`
+  display: flex;
+  margin: 10px 20px;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  height: auto;
+  background-color: white;
+  padding: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+`;
+
+const ProductImage = styled.img`
+  height: 200px;
+  width: 80%;
+  object-fit: cover;
+  margin-bottom: 10px;
+`;
+
+const ProductInfo = styled.div`
+  text-align: center;
+`;
+
+const ProductPrice = styled.p`
+  color: #5C8EF1;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 5px;
+`;
+
+const QuantityControl = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const QuantityLabel = styled.span`
+  margin-right: 10px;
+  font-weight: bold;
+`;
+
+const QuantityButton = styled.button`
+  background-color: #E0E0E0;
+  color: #333333;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
+
+const AddToCartButton = styled.button`
+  background-color: #5C8EF1;
+  color: #fff;
+  border: none;
+  font-size: 14px;
+  border-radius: 20px;
+  padding: 10px 20px;
+  margin-top: 20px;
+  cursor: pointer;
+`;
+
+const PromotionBox = styled.div`
+  width: 95%;
+  padding: 8px 0px;
+  background-color: #B6CCF7;
+  color: #333333;
+  text-align: center;
+  border-radius: 5px;
+  margin-top: -20px;
+  position: relative;
+  top: -20px; /* Place the promotion box at the top of the ProductCard */
+  left: 48%;
+  transform: translateX(-50%);
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+  z-index: 2;
+`;
+
+export default function ProductCard({product,promotion,cart}) {
+  const [quantity, setQuantity] = useState(1);
+  console.log(product)
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = (e) => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <ProductCardContainer data-testid='ProductCard'>
+      {
+        // If there is a promotion then pass the promotion details and create a promotion box on this product.
+        promotion &&
+          <PromotionBox>
+            <p style={{margin: '0',marginBottom:'8px', textDecoration:'underline', fontWeight:'200'}}>Special Promotion:</p>
+            <span style={{fontWeight:'600'}}>{promotion.title}</span>
+        </PromotionBox>
+      }
+      <ProductImage data-testid='ProductImage' src={product.image_url} alt="Product" />
+      <ProductInfo>
+        <h4 data-testid='ProductTitle'>{product.name}</h4>
+        <p data-testid='ProductCode'>Product Code: {product.code}</p>
+        <ProductPrice data-testid='ProductPrice'>{product.price}$</ProductPrice>
+        <QuantityControl>
+          <QuantityLabel>Quantity:</QuantityLabel>
+          <QuantityButton onClick={decreaseQuantity} data-testid='DecreaseButton'>-</QuantityButton>
+            <span style={{margin: '0px 5px'}} data-testid='ProductQuantity'>{quantity}</span>
+          <QuantityButton onClick={increaseQuantity} data-testid='IncreaseButton'>+</QuantityButton>
+        </QuantityControl>
+        <AddToCartButton onClick={() => cart.addItemToCart({product, quantity, promotion})} >Add to Cart</AddToCartButton>
+      </ProductInfo>
+    </ProductCardContainer>
+  )
+}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { useProdcuts } from '../context/ProductsContext';
 import { usePromotons } from '../context/PromotionsContext';
 import { useCart } from '../context/CartContext';
+import ProductCard from './ProductCard';
 
 const ProductsWrapper = styled.div`
   width: 100%;
@@ -28,102 +29,10 @@ const ProductsContainer = styled.div`
   padding: 20px;
 `;
 
-const ProductCard = styled.div`
-  display: flex;
-  margin: 10px 20px;
-  flex-direction: column;
-  align-items: center;
-  width: 300px;
-  height: auto;
-  background-color: white;
-  padding: 20px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-`;
-
-const ProductImage = styled.img`
-  height: 200px;
-  width: 80%;
-  object-fit: cover;
-  margin-bottom: 10px;
-`;
-
-const ProductInfo = styled.div`
-  text-align: center;
-`;
-
-const ProductPrice = styled.p`
-  color: #5C8EF1;
-  font-size: 20px;
-  font-weight: bold;
-  margin-top: 5px;
-`;
-
-const QuantityControl = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-`;
-
-const QuantityLabel = styled.span`
-  margin-right: 10px;
-  font-weight: bold;
-`;
-
-const QuantityButton = styled.button`
-  background-color: #E0E0E0;
-  color: #333333;
-  border: none;
-  border-radius: 5px;
-  padding: 5px 10px;
-  cursor: pointer;
-`;
-
-const AddToCartButton = styled.button`
-  background-color: #5C8EF1;
-  color: #fff;
-  border: none;
-  font-size: 14px;
-  border-radius: 20px;
-  padding: 10px 20px;
-  margin-top: 20px;
-  cursor: pointer;
-`;
-
-const PromotionBox = styled.div`
-  width: 95%;
-  padding: 8px 0px;
-  background-color: #B6CCF7;
-  color: #333333;
-  text-align: center;
-  border-radius: 5px;
-  margin-top: -20px;
-  position: relative;
-  top: -20px; /* Place the promotion box at the top of the ProductCard */
-  left: 48%;
-  transform: translateX(-50%);
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
-  z-index: 2;
-`;
-
-
-
 export default function Products() {
   const products = useProdcuts();
   const cart = useCart();
   const promotions = usePromotons();
-
-  const [quantity, setQuantity] = useState(1);
-
-  const increaseQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const decreaseQuantity = (e) => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
 
   return (
     <ProductsWrapper data-testid='ProductsWrapper'>
@@ -133,35 +42,12 @@ export default function Products() {
           products.map((product) => {
             // Find if there's any promotion for this product
             let promotion = promotions.promotions.find((promo) => promo.product_code === product.code)
-
             return(
-              <ProductCard data-testid='ProductCard'>
-                {
-                // If there is a promotion that pass the promotion details and create a promotion box on this product.
-                promotion &&
-                  <PromotionBox>
-                    <p style={{margin: '0',marginBottom:'8px', textDecoration:'underline', fontWeight:'200'}}>Special Promotion:</p>
-                    <span style={{fontWeight:'600'}}>{promotion.title}</span>
-                </PromotionBox>
-                }
-              <ProductImage data-testid='ProductImage' src={product.image_url} alt="Product" />
-              <ProductInfo>
-                <h4 data-testid='ProductTitle'>{product.name}</h4>
-                <p data-testid='ProductCode'>Product Code: {product.code}</p>
-                <ProductPrice data-testid='ProductPrice'>{product.price}$</ProductPrice>
-                <QuantityControl>
-                  <QuantityLabel>Quantity:</QuantityLabel>
-                  <QuantityButton onClick={decreaseQuantity} data-testid='DecreaseButton'>-</QuantityButton>
-                    <span style={{margin: '0px 5px'}} data-testid='ProductQuantity'>{quantity}</span>
-                  <QuantityButton onClick={increaseQuantity} data-testid='IncreaseButton'>+</QuantityButton>
-                </QuantityControl>
-                <AddToCartButton onClick={() => cart.addItemToCart({product, quantity, promotion})} >Add to Cart</AddToCartButton>
-              </ProductInfo>
-            </ProductCard>
+              <ProductCard key={product.id} product={product} promotion={promotion} cart={cart}/>
             )
           })
         }
       </ProductsContainer>
     </ProductsWrapper>
   )
-}
+};
